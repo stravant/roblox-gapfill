dae271e7775145d1c02046dd77c33fe962147eef
Mark Langen <stravant@gmail.com>
2026-02-08 01:37:36 -0800

    Update packages

--- Packages/ReactRoblox.lua
+++ Packages/ReactRoblox.lua
@@ -1,1 +1,3 @@
-return require(script.Parent._Index["jsdotlua_react-roblox@17.1.0"]["react-roblox"])
+local REQUIRED_MODULE = require(script.Parent._Index["jsdotlua_react-roblox@17.1.0"]["react-roblox"])
+export type RootType = REQUIRED_MODULE.RootType 
+return REQUIRED_MODULE
--- Packages/_Index/jsdotlua_react-roblox@17.1.0/scheduler.lua
+++ Packages/_Index/jsdotlua_react-roblox@17.1.0/scheduler.lua
@@ -1,1 +1,3 @@
-return require(script.Parent.Parent["jsdotlua_scheduler@17.1.0"]["scheduler"])
+local REQUIRED_MODULE = require(script.Parent.Parent["jsdotlua_scheduler@17.1.0"]["scheduler"])
+export type Interaction = REQUIRED_MODULE.Interaction 
+return REQUIRED_MODULE
